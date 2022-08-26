a12eb6e117d78876fe0a29545faa3b20983a1896
psbedi03 <43196173+psbedi03@users.noreply.github.com>
2022-08-26 15:24:32 -0700

    End of section 3

---
+++ -- SQLite.sql
@@ -0,0 +1,9 @@
+-- SQLite
+INSERT INTO MyProperty (Id, UserName)
+VALUES (1, "Bob");
+
+INSERT INTO MyProperty (Id, UserName)
+VALUES (2, "Tom");
+
+INSERT INTO MyProperty (Id, UserName)
+VALUES (3, "John");
