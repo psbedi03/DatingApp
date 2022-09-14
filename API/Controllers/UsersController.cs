0fa604661625d7d2bb7ded5cae65ddd31cc01a85
psbedi03 <43196173+psbedi03@users.noreply.github.com>
2022-09-14 14:18:27 -0700

    update End of Section 10

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using API.Data;
 using API.DTOs;
@@ -49,6 +50,31 @@ namespace API.Controllers
         public async Task<ActionResult<MemberDto>> GetUser(string username){
             
             return await _userRepository.GetMemberAsync(username);;
+        }
+
+        [HttpPut()]
+        public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto){
+            
+            //this will give us user's username from the token that the api uses to authenticate the user
+            var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var user = await _userRepository.GetUserByUsernameAsync(username);
+            
+            /**
+            this mapper will will add properties from memberUpdateDto to our user object
+            we don't have to manually type the following
+            
+            user.City = memberUpdateDto.City;
+            user.Country = memberUpdateDto.Country;
+            user.LookingFor = memberUpdateDto.LookingFor;
+            */
+            _mapper.Map(memberUpdateDto, user);
+
+            _userRepository.Update(user);
+
+            if(await _userRepository.SaveAllAsync()) return NoContent();
+
+            return BadRequest("Failed to update request");
+
         }
     }
 }
--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map, of } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Member } from '../_models/member';
 
@@ -10,16 +11,49 @@ import { Member } from '../_models/member';
 @Injectable({
   providedIn: 'root'
 })
+/**
+ * Services are Singletons, they are instantiated when component needs a service
+ * Thier instance stays alive unitl application is closed
+ * 
+ * So Services are good example to store application state
+ * 
+ * There are other state mngmnt solutions, such as Redux, Mobex
+ * But we will be over killing if we use them
+ * because angular provide us these services to do what we are looking for
+ */
 export class MembersService {
 
   baseUrl = environment.apiUrl;
+  members: Member[] = [];
+
   constructor(private http: HttpClient) { }
 
   getMembers(){
-    return this.http.get<Member[]>(this.baseUrl+'users')
+    if(this.members.length > 0){
+      //return as observable so that our component/client can observe this data
+      return of(this.members)
+    }
+    return this.http.get<Member[]>(this.baseUrl+'users').pipe(
+      map(members => {
+        this.members = members;
+        return members;
+      })
+    )
   }
 
   getMember(username: string){
+    const member = this.members.find(x => x.userName === username);
+    if(member !== undefined) return of(member);
+
     return this.http.get<Member>(this.baseUrl+'users/'+username);
+  }
+
+  updateMember(member: Member){
+    return this.http.put(this.baseUrl+"users", member).pipe(
+      map(() => {
+        const index = this.members.indexOf(member);
+        this.members[index] = member;
+      })
+    )
   }
 }
